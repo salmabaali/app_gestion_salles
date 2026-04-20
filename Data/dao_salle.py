ae37677a87f63c3bfc569dd2e2d5eaa3809a2924
salma <selmabaali20@gmail.com>
2026-04-20 01:27:16 -0400

    Implémentation de la classe Salle avec ses attributs et méthodes

--- Data/dao_salle.py
+++ Data/dao_salle.py
@@ -1,2 +1,8 @@
 class DataSalle:
     pass
+from models.salle import Salle
+def main():
+    s1 = Salle("A1", "Salle info", "Laboratoire", 30)
+    print(s1.afficher_infos())
+if __name__ == "__main__":
+    main()
--- models/salle.py
+++ models/salle.py
@@ -1,2 +1,14 @@
 class Salle:
     pass
+class Salle:
+    def __init__(self, code, description, categorie, capacite):
+        self.code = code
+        self.description = description
+        self.categorie = categorie
+        self.capacite = capacite
+
+    def afficher_infos(self):
+        return f"Salle {self.code} | {self.description} | {self.categorie} | Capacité: {self.capacite}"
+
+    def __str__(self):
+        return self.afficher_infos()
